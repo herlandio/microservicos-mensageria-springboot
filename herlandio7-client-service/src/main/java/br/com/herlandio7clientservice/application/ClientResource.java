9991b756dd7656fa6962b53a0d08dc5f20e369f7
herlandio <herlandio7@gmail.com>
2023-10-04 16:35:46 -0300

    add: finalizado microserviço clients

--- herlandio7-client-service/src/main/java/br/com/herlandio7clientservice/application/ClientResource.java
+++ herlandio7-client-service/src/main/java/br/com/herlandio7clientservice/application/ClientResource.java
@@ -1,15 +1,45 @@
 package br.com.herlandio7clientservice.application;
 
+import java.net.URI;
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.herlandio7clientservice.domain.Client;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("clients") // endpoint
+@RequiredArgsConstructor
 public class ClientResource {
 
-    @GetMapping
-    public String status() {
-        return "ok";
+    private final ClientService service;
+
+    @PostMapping
+    public ResponseEntity<Client> save(@RequestBody ClientSaveRequest request) {
+        var client = request.toModel();
+        service.save(client);
+        URI headerLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .query("cpf={cpf}")
+                .buildAndExpand(client.getCpf())
+                .toUri();
+        return ResponseEntity.created(headerLocation).build();
+    }
+
+    @GetMapping(params = "cpf")
+    public ResponseEntity<Optional<Client>> dataClient(@RequestParam("cpf") String cpf) {
+        var client = service.getByCPF(cpf);
+        if (client.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(client);
     }
 }
---
+++ herlandio7-client-service/src/main/java/br/com/herlandio7clientservice/application/ClientSaveRequest.java
@@ -0,0 +1,15 @@
+package br.com.herlandio7clientservice.application;
+
+import br.com.herlandio7clientservice.domain.Client;
+import lombok.Data;
+
+@Data // ja realiza os get and sets
+public class ClientSaveRequest {
+    private String cpf;
+    private String name;
+    private Integer age;
+
+    public Client toModel() {
+        return new Client(cpf, name, age);
+    }
+}
