5bf72814e24299e84d1a013cb8c32dc76cf70983
herlandio <herlandio7@gmail.com>
2023-10-04 12:19:03 -0300

    começando com eureka server

---
+++ herlandio7-eureka-server/src/main/java/br/com/herlandio7eurekaserver/Herlandio7EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.herlandio7eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Herlandio7EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Herlandio7EurekaServerApplication.class, args);
+	}
+
+}
---
+++ herlandio7-eureka-server/src/test/java/br/com/herlandio7eurekaserver/Herlandio7EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.herlandio7eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Herlandio7EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
