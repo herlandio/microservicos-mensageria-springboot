e43fa366b8c1a767d899bcad3b91bd3d0d675145
herlandio <herlandio7@gmail.com>
2023-10-04 13:05:27 -0300

    add: começando client service

---
+++ herlandio7-client-service/src/main/java/br/com/herlandio7clientservice/Herlandio7ClientServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.herlandio7clientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Herlandio7ClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Herlandio7ClientServiceApplication.class, args);
+	}
+
+}
---
+++ herlandio7-client-service/src/test/java/br/com/herlandio7clientservice/Herlandio7ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.herlandio7clientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Herlandio7ClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
