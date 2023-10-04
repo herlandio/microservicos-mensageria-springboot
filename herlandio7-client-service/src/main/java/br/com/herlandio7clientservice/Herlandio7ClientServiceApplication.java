8e87eea704cd4ce0508f09bd34cb89b4a63b4e12
herlandio <herlandio7@gmail.com>
2023-10-04 13:53:24 -0300

    add: registrando o microserviço de clientes

--- herlandio7-client-service/src/main/java/br/com/herlandio7clientservice/Herlandio7ClientServiceApplication.java
+++ herlandio7-client-service/src/main/java/br/com/herlandio7clientservice/Herlandio7ClientServiceApplication.java
@@ -2,8 +2,10 @@ package br.com.herlandio7clientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Herlandio7ClientServiceApplication {
 
 	public static void main(String[] args) {
