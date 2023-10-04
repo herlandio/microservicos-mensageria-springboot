1c873f00b04f81917892060c6b50c447d62e997f
herlandio <herlandio7@gmail.com>
2023-10-04 17:14:06 -0300

    add: implementando o cloud gateway

---
+++ herlandio7-cloud-gateway/src/test/java/br/com/herlandio7cloudgateway/Herlandio7CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.herlandio7cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Herlandio7CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
